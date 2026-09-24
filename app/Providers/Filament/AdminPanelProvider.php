a1bbb3885a56612861717b5661d1e69f7e804298
Maksim Tsatsura <maxim.tsatsura@gmail.com>
2026-09-24 14:50:18 +0300

    feat: Clean AdminPanelProvider

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Providers\Filament;
 
-use BezhanSalleh\FilamentShield\FilamentShieldPlugin;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -24,11 +23,8 @@ use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
-use LaraZeus\SpatieTranslatable\SpatieTranslatablePlugin;
 use Nicole\Box\Core\NicoleCorePlugin;
-use Valerie\Box\IndustryCctv\ValerieCctvPlugin;
 use Valerie\Box\IndustryShowers\ValerieShowersPlugin;
-use Outerweb\FilamentTranslatableFields\TranslatableFieldsPlugin;
 
 class AdminPanelProvider extends PanelProvider
 {
@@ -40,7 +36,7 @@ class AdminPanelProvider extends PanelProvider
       ->path('admin')
       ->login()
       ->colors([
-        'primary' => Color::Blue,
+        'primary' => Color::Teal,
       ])
       ->maxContentWidth(Width::Full)
       ->brandLogo(asset('images/logo.svg'))
@@ -75,35 +71,15 @@ class AdminPanelProvider extends PanelProvider
         fn (): string => Blade::render('@vite(["resources/css/app.css", "resources/js/app.tsx"])')
       )
       ->navigationGroups([
-        NavigationGroup::make()
-          ->label(fn (): string => __('Catalog')),
-
-        NavigationGroup::make()
-          ->label(fn (): string => __('Configurations')),
-
-        NavigationGroup::make()
-          ->label(fn (): string => __('Catalog Settings'))
-          ->collapsed(),
-
-        NavigationGroup::make()
-          ->label(fn (): string => __('Inventory')),
-
-        NavigationGroup::make()
-          ->label(fn (): string => __('Access Control'))
-          ->collapsed(),
+        NavigationGroup::make()->label(fn (): string => __('Catalog')),
+        NavigationGroup::make()->label(fn (): string => __('Configurations')),
+        NavigationGroup::make()->label(fn (): string => __('Catalog Settings'))->collapsed(),
+        NavigationGroup::make()->label(fn (): string => __('Inventory')),
+        NavigationGroup::make()->label(fn (): string => __('Access Control'))->collapsed(),
       ])
       ->plugins([
         NicoleCorePlugin::make(),
-
         ValerieShowersPlugin::make(),
-
-        SpatieTranslatablePlugin::make()
-          ->defaultLocales(['ru']),
-
-        TranslatableFieldsPlugin::make()
-          ->supportedLocales(['ru']),
-
-        FilamentShieldPlugin::make()->navigationGroup('Контроль доступа'),
       ]);
   }
 }
