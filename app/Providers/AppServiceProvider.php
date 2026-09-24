1da57ab11b0f9c8f6fccf203a59488bb32a16413
Maksim Tsatsura <maxim.tsatsura@gmail.com>
2026-09-24 14:16:35 +0300

    fix: page polices

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,25 +7,16 @@ use Illuminate\Support\Facades\Gate;
 
 class AppServiceProvider extends ServiceProvider
 {
-  public function register(): void
-  {
-    //
-  }
+    public function register(): void
+    {
+        //
+    }
 
-  public function boot(): void
-  {
-    // Глобальный супер-админ (доступ ко всему)
-    Gate::before(function ($user, $ability) {
-      return $user->hasRole('admin') ? true : null;
-    });
-
-    // Связываем модели ядра Nicole Core с политиками в App\Policies
-    Gate::guessPolicyNamesUsing(function (string $modelClass) {
-      $classBasename = class_basename($modelClass);
-      $policyClass = "App\\Policies\\{$classBasename}Policy";
-
-      return class_exists($policyClass) ? $policyClass : null;
-    });
-  }
-
+    public function boot(): void
+    {
+        // Глобальный супер-админ (доступ ко всему)
+        Gate::before(function ($user, $ability) {
+            return $user->hasRole('admin') ? true : null;
+        });
+    }
 }
